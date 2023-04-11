8d843e27e11a90352e272143176d79adfe57a366
HADES <1005710179@qq.com>
2023-04-11 11:55:59 +0800

    feat: 异常拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './interceptor/response/response.interceptor';
+import { HttpFilterFilter } from './filter/http-filter/http-filter.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpFilterFilter());
   await app.listen(4000);
 }
 bootstrap();
