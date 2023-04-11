f26229bb2780f6f147eb60bea4694029ce4150d1
HADES <1005710179@qq.com>
2023-04-11 13:08:51 +0800

    feat: Swagger 文档

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './interceptor/response/response.interceptor';
 import { HttpFilterFilter } from './filter/http-filter/http-filter.filter';
+import Swagger from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpFilterFilter());
+  new Swagger(app);
   await app.listen(4000);
 }
 bootstrap();
---
+++ src/utils/swagger.ts
@@ -0,0 +1,13 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger/dist';
+export default class Swagger {
+  constructor(app) {
+    const config = new DocumentBuilder()
+      .setTitle('接口文档')
+      .setDescription('描述，。。。')
+      .setVersion('1.0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api-docs', app, document);
+  }
+}
