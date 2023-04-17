71a25288d6ee3ed1acfd8ea4ca5506af79af974b
HADES <1005710179@qq.com>
2023-04-17 17:35:51 +0800

    feat: 数据库连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: '123456',
+      host: 'localhost',
+      port: 3306,
+      database: 'db_nest',
+      synchronize: true, //是否自动将实体类同步到数据库
+      retryDelay: 500,
+      retryAttempts: 10, //重试连接数据库的次数
+      autoLoadEntities: true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
